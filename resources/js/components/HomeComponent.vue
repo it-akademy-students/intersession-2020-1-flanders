9571c2100eba7164ee0027661a39a0c1e194a82f
Steph-38 <stephchevalier38@gmail.com>
2020-08-21 13:33:12 +0000

    Récupération des données fichiers php via l'api github

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Composant Home</h1>
+        <div class="form-group">
+            <label for="inputGitHubUrl">Lien du repo GitHub</label>
+            <input type="url" id="inputGitHubUrl" class="form-control" aria-describedby="HelpBlock" v-model="url">
+            <small id="HelpBlock" class="form-text text-muted">Veuillez insérer le lien vers le repo GitHub contenant des fichiers PHP à scanner</small>
+            <button type="submit" class="btn btn-primary mt-2" @click="checkUrl">Scanner</button>
+        </div>
+        {{info}}
+    </div>
+</template>
+
+<script>
+    export default {
+
+    data() {
+        return {
+            url: '',
+            info: null
+        }
+    },
+
+    methods: {
+        checkUrl() {
+            let url = this.url;
+            let url2 = url.slice(19);
+            // let url3 = `https://api.github.com/repos/${url2}`;
+            let url3 = `https://api.github.com/search/code?q=.php+in:path+repo:${url2}`;
+            console.log(url3);
+                axios.get(`${url3}`)
+                    .then(response => {
+                        this.info = response;
+                        // this.info = response.data.map(i => i.name);
+            });
+
+            
+
+            // axios.get('https://192.168.33.10/', {
+            //     url: this.url
+            // })
+        }
+    },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,26 +8,33 @@
     </head>
     <body>
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Check Your PHP</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarColor02">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
-                </li>
-              </ul>
-            </div>
-        </nav>
+        <div id="app">
+            <!-- Barre de navigation -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+				<router-link to="/" class="navbar-brand">Check Your PHP</router-link>
+                {{-- <a class="navbar-brand" href="#">Check Your PHP</a> --}}
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor02">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+						<router-link to="/example" class="nav-link">Example</router-link>
+					</li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+            </nav>
+			<div class="container mt-2">
+				<!-- Composants Vue gérés par VueRouter -->
+				<router-view></router-view>
+			</div>
+        </div>
 
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
