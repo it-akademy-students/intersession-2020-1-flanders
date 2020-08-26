ae7d1df39732be93eb99503e0db8d10e1917e3a6
alexandre <alexandre.moreau@live.com>
2020-08-26 11:37:13 +0200

    requete guzzle fonctionnel, manque décryptage base64 fichier + token d authentification pour avoir plus de 60 requete/heure

--- app/Http/Controllers/ProcessFilesController.php
+++ app/Http/Controllers/ProcessFilesController.php
@@ -12,10 +12,29 @@ use Storage;
 class ProcessFilesController extends BaseController
 {
     public function index(Request $request){
-        foreach( $request->params as $file){
-            $filename =  Storage::disk('public')->put('files/' . $storeFile);
-            return $file;
-        }
-        return $request->params;
+//https://github.com/it-akademy-students/intersession-2020-1-flanders
+  // $filename =  Storage::disk('public')->put('files/fileTest.php' , 'caca');
+
+foreach( $request->params as $key => $file) {
+          $owner = $file['repository']['owner']['login'];
+          $repo = $file['repository']['full_name'];
+          $explode = explode('/', $repo);
+          $realRepo = $explode[1];
+          $path = $file['path'];
+          $client = new \GuzzleHttp\Client();
+          $return = $client->get('https://api.github.com/repos/'.$owner.'/'.$realRepo.'/contents/'.$path.'');
+          $response = $return->getBody();
+          $filename =  Storage::disk('public')->put('files/'.$key , $response);
+          // return $return;
+      }
+      return $request->params;
+
+      //
+      // return $request->params;
+      //   foreach( $request->params as $file){
+      //       // $filename =  Storage::disk('public')->put('files/' . $storeFile);
+      //       return $file;
+      //   }
+        // return $request->params;
     }
 }
