41212e3cebf7ff4acb5cf4192dbd0a6e23d9d40c
alexandre <alexandre.moreau@live.com>
2020-09-03 09:58:04 +0200

    backAmélio, email avec attachment easy :p

---
+++ resources/views/mails/email2.blade.php
@@ -0,0 +1,61 @@
+ ------ ------------------------------------------------------------------------------------------------------------------ 
+  Line   Authenticate.php                                                                                                  
+ ------ ------------------------------------------------------------------------------------------------------------------ 
+  17     Method App\Http\Middleware\Authenticate::redirectTo() should return string|null but return statement is missing.  
+ ------ ------------------------------------------------------------------------------------------------------------------ 
+
+ ------ ------------------------------------------------------------------------------------------------------- 
+  Line   Handler.php                                                                                            
+ ------ ------------------------------------------------------------------------------------------------------- 
+  15     Property App\Exceptions\Handler::$dontReport type has no value type specified in iterable type array.  
+         💡 Consider adding something like array<Foo> to the PHPDoc.                                             
+         You can turn off this check by setting checkMissingIterableValueType: false in your                    
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.                              
+  24     Property App\Exceptions\Handler::$dontFlash type has no value type specified in iterable type array.   
+         💡 Consider adding something like array<Foo> to the PHPDoc.                                             
+         You can turn off this check by setting checkMissingIterableValueType: false in your                    
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.                              
+ ------ ------------------------------------------------------------------------------------------------------- 
+
+ ------ ------------------------------------------------------------------------------------------------------ 
+  Line   Kernel.php                                                                                            
+ ------ ------------------------------------------------------------------------------------------------------ 
+  16     Property App\Http\Kernel::$middleware type has no value type specified in iterable type array.        
+         💡 Consider adding something like array<Foo> to the PHPDoc.                                            
+         You can turn off this check by setting checkMissingIterableValueType: false in your                   
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.                             
+  31     Property App\Http\Kernel::$middlewareGroups type has no value type specified in iterable type array.  
+         💡 Consider adding something like array<Foo> to the PHPDoc.                                            
+         You can turn off this check by setting checkMissingIterableValueType: false in your                   
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.                             
+  55     Property App\Http\Kernel::$routeMiddleware type has no value type specified in iterable type array.   
+         💡 Consider adding something like array<Foo> to the PHPDoc.                                            
+         You can turn off this check by setting checkMissingIterableValueType: false in your                   
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.                             
+ ------ ------------------------------------------------------------------------------------------------------ 
+
+ ------ --------------------------------------------------------------------------------------- 
+  Line   User.php                                                                               
+ ------ --------------------------------------------------------------------------------------- 
+  18     Property App\User::$fillable type has no value type specified in iterable type array.  
+         💡 Consider adding something like array<Foo> to the PHPDoc.                             
+         You can turn off this check by setting checkMissingIterableValueType: false in your    
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.              
+  27     Property App\User::$hidden type has no value type specified in iterable type array.    
+         💡 Consider adding something like array<Foo> to the PHPDoc.                             
+         You can turn off this check by setting checkMissingIterableValueType: false in your    
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.              
+  36     Property App\User::$casts type has no value type specified in iterable type array.     
+         💡 Consider adding something like array<Foo> to the PHPDoc.                             
+         You can turn off this check by setting checkMissingIterableValueType: false in your    
+         /var/www/html/laravelSess/intersession-2020-1-flanders/phpstan.neon.dist.              
+ ------ --------------------------------------------------------------------------------------- 
+
+ ------ --------------------------- 
+  Line   console.php                
+ ------ --------------------------- 
+  18     Undefined variable: $this  
+ ------ --------------------------- 
+
+ [ERROR] Found 10 errors                                                                                                
+
