c703c00423def7067f2d813c9b2a81e89f6b270b
Steph-38 <44949858+Steph-38@users.noreply.github.com>
2020-09-04 10:13:56 +0200

    Update GetStartedComponent.vue

--- resources/js/components/GetStartedComponent.vue
+++ resources/js/components/GetStartedComponent.vue
@@ -2,11 +2,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Fonctionnement</div>
-
+                <div class="card text-white bg-dark mb-3">
+                    <div class="card-header"><h4>Fonctionnement</h4></div>
                     <div class="card-body">
-                        Ce site permet d'éxécuter des outils d'analyse sur un projet. Il suffit de renseigner le lien du projet github ainsi qu'un email dans la section "Analyse du code PHP" et les rapports d'analyse vous seront envoyés.
+                        <p class="card-text">Ce site permet d'éxécuter des outils d'analyse sur un projet. Il suffit de renseigner le lien du projet github ainsi qu'un email dans la section "Analyse du code PHP" et les rapports d'analyse vous seront envoyés.</p>
                     </div>
                 </div>
             </div>
